d9ff73cee12d6c7242a8e36a838e7799b19f44f7
Bino Bense <binobense075@gmail.com>
2025-08-02 13:19:53 +0530

    initialize our project and bundling/ our project with parcer

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "Hot module replacement"),
     React.createElement("h2", {}, "i'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
