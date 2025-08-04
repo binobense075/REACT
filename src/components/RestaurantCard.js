34450f3a7dd31fd17c153847043ebd4949f6f144
Bino Bense <binobense075@gmail.com>
2025-08-04 19:44:59 +0530

    changes in restrocard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,7 +4,8 @@ const RestaurantCard = ({
   name,
   cuisines,
   avgRating,
-  lastMileTravelString,
+  costForTwo,
+  sla,
   cloudinaryImageId,
 }) => {
   return (
@@ -16,8 +17,9 @@ const RestaurantCard = ({
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}</h4>
-      <h4>{lastMileTravelString}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} mins</h4>
     </div>
   );
 };
